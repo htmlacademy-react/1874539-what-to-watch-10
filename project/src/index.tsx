b901cc2f85bf965ffe5127dd3491c895014e58be
Kulimzin Sergei <kulimzin.sergei@gmail.com>
2022-07-10 23:26:23 +0300

    add component MainScreen

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const filmPromo = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  releaseDate: new Date(2014, 2, 13),
+};
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App filmPromo={filmPromo} />
+  </React.StrictMode>
 );
