370f81cbe092a610ec195f7df05ab6865004aaca
Kulimzin Sergei <kulimzin.sergei@gmail.com>
2022-07-14 15:06:53 +0300

    add routing with react-router-dom

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import FilmScreen from '../../pages/film-screen/film-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+
+import { AppRoute } from '../../const';
 
 type AppProps = {
   filmPromo: {
@@ -9,7 +19,19 @@ type AppProps = {
 };
 
 function App({ filmPromo }: AppProps): JSX.Element {
-  return <MainScreen filmPromo={filmPromo} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainScreen filmPromo={filmPromo} />} />
+        <Route path={AppRoute.SignIn} element={<SignInScreen />} />
+        <Route path={AppRoute.MyList} element={<MyListScreen />} />
+        <Route path={AppRoute.Film} element={<FilmScreen />} />
+        <Route path={AppRoute.AddReview} element={<AddReviewScreen />} />
+        <Route path={AppRoute.Player} element={<PlayerScreen />} />
+        <Route path={AppRoute.NotFound} element={<NotFoundScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
