66e5d993c36e9411c3fa908cb58628221bff3ffe
Kulimzin Sergei <kulimzin.sergei@gmail.com>
2022-07-17 21:16:54 +0300

    add features: random film promo

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import FilmsList from '../../components/films-list/films-list';
 import Logo from '../../components/logo/logo';
 import PageFooter from '../../components/page-footer/page-footer';
@@ -5,22 +6,29 @@ import PageFooter from '../../components/page-footer/page-footer';
 import { Film } from '../../types/film';
 
 type MainScreenProps = {
-  filmPromo: {
-    title: string;
-    genre: string;
-    releaseDate: Date;
-  };
-
   films: Film[];
 };
 
-function MainScreen({ filmPromo, films }: MainScreenProps): JSX.Element {
+function MainScreen({ films }: MainScreenProps): JSX.Element {
+  const getRandomFilmPromo = () => {
+    if (films.length) {
+      const randomIndex = Math.floor(Math.random() * (films.length - 1));
+      return films[randomIndex];
+    }
+
+    return null;
+  };
+
+  const randomFilm = getRandomFilmPromo();
+
   return (
     <>
       <section className='film-card'>
-        <div className='film-card__bg'>
-          <img src='img/bg-the-grand-budapest-hotel.jpg' alt='The Grand Budapest Hotel' />
-        </div>
+        {randomFilm && (
+          <div className='film-card__bg'>
+            <img src={randomFilm.images.posterUrl} alt={randomFilm.title} />
+          </div>
+        )}
         <h1 className='visually-hidden'>WTW</h1>
         <header className='page-header film-card__head'>
           <Logo />
@@ -31,44 +39,43 @@ function MainScreen({ filmPromo, films }: MainScreenProps): JSX.Element {
               </div>
             </li>
             <li className='user-block__item'>
-              <a className='user-block__link'>Sign out</a>
+              <Link className='user-block__link' to='/login'>
+                Sign out
+              </Link>
             </li>
           </ul>
         </header>
-        <div className='film-card__wrap'>
-          <div className='film-card__info'>
-            <div className='film-card__poster'>
-              <img
-                src='img/the-grand-budapest-hotel-poster.jpg'
-                alt='The Grand Budapest Hotel poster'
-                width={218}
-                height={327}
-              />
-            </div>
-            <div className='film-card__desc'>
-              <h2 className='film-card__title'>{filmPromo.title}</h2>
-              <p className='film-card__meta'>
-                <span className='film-card__genre'>{filmPromo.genre}</span>
-                <span className='film-card__year'>{filmPromo.releaseDate.getFullYear()}</span>
-              </p>
-              <div className='film-card__buttons'>
-                <button className='btn btn--play film-card__button' type='button'>
-                  <svg viewBox='0 0 19 19' width={19} height={19}>
-                    <use xlinkHref='#play-s' />
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className='btn btn--list film-card__button' type='button'>
-                  <svg viewBox='0 0 19 20' width={19} height={20}>
-                    <use xlinkHref='#add' />
-                  </svg>
-                  <span>My list</span>
-                  <span className='film-card__count'>{films.length}</span>
-                </button>
+        {randomFilm && (
+          <div className='film-card__wrap'>
+            <div className='film-card__info'>
+              <div className='film-card__poster'>
+                <img src={randomFilm.images.posterUrl} alt={randomFilm.title} width={218} height={327} />
+              </div>
+              <div className='film-card__desc'>
+                <h2 className='film-card__title'>{randomFilm.title}</h2>
+                <p className='film-card__meta'>
+                  <span className='film-card__genre'>{randomFilm.genres.toString()}</span>
+                  <span className='film-card__year'>{randomFilm.releaseYear}</span>
+                </p>
+                <div className='film-card__buttons'>
+                  <Link className='btn btn--play film-card__button' to={`/player/${randomFilm.id}`}>
+                    <svg viewBox='0 0 19 19' width={19} height={19}>
+                      <use xlinkHref='#play-s' />
+                    </svg>
+                    <span>Play</span>
+                  </Link>
+                  <Link className='btn btn--list film-card__button' to='/mylist'>
+                    <svg viewBox='0 0 19 20' width={19} height={20}>
+                      <use xlinkHref='#add' />
+                    </svg>
+                    <span>My list</span>
+                    <span className='film-card__count'>{films.length}</span>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </section>
       <div className='page-content'>
         <section className='catalog'>
