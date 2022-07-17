84423927f11032e4e770bd8fbe756a10195416c7
Kulimzin Sergei <kulimzin.sergei@gmail.com>
2022-07-17 19:04:05 +0300

    add component FilmsList

---
+++ project/src/components/films-list/films-list.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import FilmCard from '../film-card/film-card';
+
+import { Film } from '../../types/film';
+
+type FilmsListProps = {
+  films: Film[];
+};
+
+function FilmsList({ films }: FilmsListProps): JSX.Element | null {
+  const [activeFilmCardId, setActiveFilmCardId] = useState<string>('');
+
+  const makeFilmCardActive = (id: string) => {
+    setActiveFilmCardId(id);
+  };
+
+  if (films.length) {
+    return (
+      <div className='catalog__films-list'>
+        {films.map((film) => (
+          <FilmCard
+            key={film.id}
+            makeFilmCardActive={makeFilmCardActive}
+            isActive={film.id === activeFilmCardId}
+            {...film}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+}
+
+export default FilmsList;
